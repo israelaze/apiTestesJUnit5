d130d44c78feec52843c3ef6ba3d5313fc69808d
israelaze <israelaze@gmail.com>
2023-10-05 09:11:43 -0300

    Classe User

---
+++ src/main/java/br/com/cursojunit/api/domain/User.java
@@ -0,0 +1,24 @@
+package br.com.cursojunit.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
