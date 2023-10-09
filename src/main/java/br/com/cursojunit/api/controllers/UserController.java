4859971016c7ac855f78f103e14e6c42018b18db
israelaze <israelaze@gmail.com>
2023-10-09 16:59:20 -0300

    classe UserController findById

--- src/main/java/br/com/cursojunit/api/controllers/UserController.java
+++ src/main/java/br/com/cursojunit/api/controllers/UserController.java
@@ -1,6 +1,8 @@
 package br.com.cursojunit.api.controllers;
 
 import br.com.cursojunit.api.domain.User;
+import br.com.cursojunit.api.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
+@AllArgsConstructor
 public class UserController {
 
+    private final UserService service;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Edy Silva", "edy@bol.com", "1234"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
