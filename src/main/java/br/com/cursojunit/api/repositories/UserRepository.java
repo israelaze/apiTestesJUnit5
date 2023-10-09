2b168930c5d73caa0293fe273ee41b7ae2dd5880
israelaze <israelaze@gmail.com>
2023-10-09 16:57:49 -0300

    interface UserRepository

---
+++ src/main/java/br/com/cursojunit/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.cursojunit.api.repositories;
+
+import br.com.cursojunit.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
