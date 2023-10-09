6eb11b91fb7b4d0ae64f927a732d07e6093b44fd
israelaze <israelaze@gmail.com>
2023-10-09 16:59:08 -0300

    classe UserServiceImpl findById

---
+++ src/main/java/br/com/cursojunit/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.cursojunit.api.services.impl;
+
+import br.com.cursojunit.api.domain.User;
+import br.com.cursojunit.api.repositories.UserRepository;
+import br.com.cursojunit.api.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+    @Override
+    public User findById(Integer id) {
+
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
