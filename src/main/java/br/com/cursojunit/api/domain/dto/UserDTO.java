1f6cbb8ef8ed675438a5ea720b90be609b455f6d
israelaze <israelaze@gmail.com>
2023-10-09 18:07:30 -0300

    User dto

---
+++ src/main/java/br/com/cursojunit/api/domain/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package br.com.cursojunit.api.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
